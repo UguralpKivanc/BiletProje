ecabaa68e822a88fe3516a27a901720094be34f5
UguralpKivanc <kivancuguralp@gmail.com>
2026-03-16 21:07:53 +0300

    .

--- BiletProje/obj/Release/net8.0/BiletProje.AssemblyInfo.cs
+++ BiletProje/obj/Release/net8.0/BiletProje.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiletProje")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991ba3bf3fa25fe6e4451bb4d1ca0aebab2882c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiletProje")]
