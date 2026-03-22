562f1f581c0e69da113d899f533120a837008559
UguralpKivanc <kivancuguralp@gmail.com>
2026-03-23 00:20:13 +0300

    TDD [Red Phase]
    Henüz GatewayController içerisinde yönlendirme mantığı kurulmadığı için testlerin başarısız olması (Red Phase) beklenmektedir.

--- Dispatcher.Tests/DispatcherRoutingTests.cs
+++ Dispatcher.Tests/DispatcherRoutingTests.cs
@@ -1,33 +1,31 @@
 ﻿using Xunit;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks; // async Task için gerekli
+using System.Threading.Tasks;
 using Dispatcher.Controllers;
 
 namespace Dispatcher.Tests
 {
     public class DispatcherRoutingTests
     {
-        // 1. Önceki testin
         [Fact]
-        public void Dispatcher_Should_Return_NotFound_For_Invalid_Route()
+        public async Task Dispatcher_Should_Return_NotFound_For_Invalid_Route()
         {
             // Arrange
             var controller = new GatewayController();
 
             // Act
-            var result = controller.Get("/invalid-path");
+            var result = await controller.ForwardToService("/invalid-path");
 
             // Assert
             Assert.IsType<NotFoundResult>(result);
         }
 
-        // 2. Yeni eklediğin testin (Burası sınıfın içi)
         [Fact]
         public async Task Dispatcher_Should_Forward_Events_Request_To_EventService()
         {
             // Arrange
             var controller = new GatewayController();
-            string path = "/events";
+            string path = "events";
 
             // Act
             var result = await controller.ForwardToService(path);
